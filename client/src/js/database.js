8cfc26e93bb001cf0e31c1c5741d476a3e30f93b
Newton Hoang <newtonhoang99@gmail.com>
2022-02-05 23:01:33 -0800

    js/src work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,42 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try
+  {
+    console.log(content);
+
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ content: content, id: 1});
+    const result = await request;
+
+    console.log('Successfully saved to database', result);
+  }
+  catch(err) {
+    console.error('putDb Error', err);
+  }
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try
+  {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+
+    console.log('result.value', result[0].content);
+    
+    return result[0].content;
+  }
+  catch(err) {
+    console.error('getDb Error', err);
+  }
+
+}
 
 initdb();
